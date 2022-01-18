a500808c87598f7c2da02aed22d33dd3d3a15473
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2022-01-18 23:11:25 +0100

    Convert code from JS to jQuery

--- skripta.js
+++ skripta.js
@@ -1,11 +1,11 @@
 function izracunaj() {
-    var a = document.getElementById("vrs").value;
-    var b = document.getElementById("gd").value;
-    var c = document.getElementById("mr").value;
-    var d = document.getElementById("tp").value;
-    var e = document.getElementById("ts").value;
-    var f = document.getElementById("tpr").value;
-    var g = document.getElementById("str").value;
+    var a = $("#vrs").val();
+    var b = $("#gd").val();
+    var c = $("#mr").val();
+    var d = $("#tp").val();
+    var e = $("#ts").val();
+    var f = $("#tpr").val();
+    var g = $("#str").val();
 
     if (c <= 0 || c == "e" ||  d <= 0 || d == "e" || e == "e" || f == "e" ) {
         alert("Unos nije dozvoljen.\nMolimo vas da pažljivo pročitate uputstvo.");
@@ -52,26 +52,26 @@ function izracunaj() {
     if (e >= 20 && b == 0.06) {
         lu = l * b;
         l1 = l + lu;
-        document.getElementById("rezultat6").innerHTML = "Potrebna količina rashladnog sredstva, koja je uvećana za 6% u ljetnim mjesecima, iznosi " + l1.toFixed(0) + "[kg].";
+        $("#rezultat6").html("Potrebna količina rashladnog sredstva, koja je uvećana za 6% u ljetnim mjesecima, iznosi " + l1.toFixed(0) + "[kg].");
     }
 
-        document.getElementById("rezultat").innerHTML = "Potrebna količina rashadnog sredstva za hlađenje vagona hladnjače je " + q11.toFixed(0) + " [kg]";
+        $("#rezultat").html("Potrebna količina rashadnog sredstva za hlađenje vagona hladnjače je " + q11.toFixed(0) + " [kg].");
 
         if (q22 != 0) {
-            document.getElementById("rezultat2").innerHTML = "Potrebna količina rashadnog sredstva za hlađenje robe je " + q22.toFixed(0) + " [kg]";
+            $("#rezultat2").html("Potrebna količina rashadnog sredstva za hlađenje robe je " + q22.toFixed(0) + " [kg].");
         } else {
-            document.getElementById("rezultat2").innerHTML = "Nije potrebno računari koliko je potrebno rashladnog sredstva za hlađenje robe, jer je roba prethodno ohlađena.";
+            $("#rezultat2").html("Nije potrebno računari koliko je potrebno rashladnog sredstva za hlađenje robe, jer je roba prethodno ohlađena.");
         }
 
-        document.getElementById("rezultat3").innerHTML = "Potrebna količina rashadnog sredstva za uticaj spoljne temperature je " + q33.toFixed(0) + " [kg]";
+        $("#rezultat3").html("Potrebna količina rashadnog sredstva za uticaj spoljne temperature je " + q33.toFixed(0) + " [kg].");
 
         if (q43 != 0) {
-            document.getElementById("rezultat4").innerHTML = "Potrebna količina rashadnog sredstva za uticaj disanja robe je " + q43.toFixed(0) + " [kg]";
+            $("#rezultat4").html("Potrebna količina rashadnog sredstva za uticaj disanja robe je " + q43.toFixed(0) + " [kg].");
         } else if (q43 = 0 && f <= -19 && f >= -40) {
-            document.getElementById("rezultat4").innerHTML = "Nije potrebno računati potrebnu količinu rashladnog sredstva za uticaj disanja robe, jer roba u brzo smrznutom stanju &quot;ne diše&quot;."
+            $("#rezultat4").html("Nije potrebno računati potrebnu količinu rashladnog sredstva za uticaj disanja robe, jer roba u brzo smrznutom stanju &quot;ne diše&quot;.");
         } else {
-            document.getElementById("rezultat4").innerHTML= "Nije potrebno računati potrebnu količinu rashladnog sredstva za uticaj disanja robe, jer roba u smrznutom stanju &quot;ne diše&quot;."
+            $("#rezultat4").html("Nije potrebno računati potrebnu količinu rashladnog sredstva za uticaj disanja robe, jer roba u smrznutom stanju &quot;ne diše&quot;.");
         }
         
-        document.getElementById("rezultat5").innerHTML = "Potrebna količina rashladnog sredstva iznosi " + l + "[kg].";
+        $("#rezultat5").html("Potrebna količina rashladnog sredstva iznosi " + l + "[kg].");
 }
